827cc2e54fd368328dbe3590e8cf69c45928774e
boy wonder <wonderapomah@gmail.com>
2026-09-08 01:58:13 -0900

    added changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-import PayButton from "./components/PayButton";s
+import PayButton from "./components/PayButton";
 
 type Message = {
   role: "user" | "ai";
